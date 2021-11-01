98972d24805da0dd837671f348bab620932dbe8f
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-11-01 20:28:00 -0300

    Lógica para finalizar o app

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const questions = [
   "Quantas pessoas ajudei hoje?"
 ]
 const ask = (index = 0) => {
-  process.stdout.write(questions[index] + "\n\n\n")
+  process.stdout.write("\n\n\n" + questions[index] + " ->")
 }
 
 ask()
-
+const answers = [];
 process.stdin.on("data", data => {
-  process.stdout.write(data.toString().trim() + '\n')
-  process.exit()
+  answers.push(data.toString().trim())
+  if(answers.length < questions.length){
+    ask(answers.length)
+  }else{
+    console.log(answers)
+    process.exit()
+
+  }
 })
