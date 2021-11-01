e59abd002e3b71edf89c0d55b87038a3abcac4d0
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-11-01 19:14:32 -0300

    process.stdout.write()

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Rodando via script")
+const questions = [
+  "O que aprendi hoje?",
+  "O que me deixou aborrecido?",
+  "O que eu poderia fazer para melhorar?",
+  "O que me deixou feliz hoje?",
+  "Quantas pessoas ajudei hoje?"
+]
+const ask = (index = 0) => {
+  process.stdout.write(questions[index] + "\n\n\n")
+}
+
+ask()
