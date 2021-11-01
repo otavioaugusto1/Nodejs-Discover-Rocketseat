5a0252a6af8cf9a46783ab85ad70841b0b06303b
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-11-01 14:34:06 -0300

    meu próprio módulo

--- require.js
+++ require.js
@@ -1,4 +1,8 @@
 // require serve para trazer módulos usando node
-
+//módulos nativos
 const path = require('path')
 console.log(path.basename(__filename))
+
+//meu módulo
+const myModule = require('./exports')
+console.log(myModule)
